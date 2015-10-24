b61c6286c0a471165e070bb48e9c47712d3ce431
urb4nm <urb4nm@gmail.com>
2015-10-24 21:27:40 +0200

    Some minor configuration

--- AutomatedTellerMachine/Controllers/HomeController.cs
+++ AutomatedTellerMachine/Controllers/HomeController.cs
@@ -8,11 +8,13 @@ namespace AutomatedTellerMachine.Controllers
 {
     public class HomeController : Controller
     {
+        // GET /home/index
         public ActionResult Index()
         {
             return View();
         }
-
+        
+        //GET /home/about
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,11 +22,24 @@ namespace AutomatedTellerMachine.Controllers
             return View();
         }
 
+        //GET /home/contact
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVC5ATM1";
+
+            if (letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            //return Content(serial);
+            return Json(new { name = "serial", value = serial }, JsonRequestBehavior.AllowGet);
         }
     }
 }
