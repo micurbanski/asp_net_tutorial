59ed04a08d39f034442d2e35e01ffb904486d375
urb4nm <urb4nm@gmail.com>
2015-10-26 13:26:09 +0100

    First Model, Detail and Create Views

---
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutomatedTellerMachine.Models
+{
+    public class CheckingAccount
+    {
+        public int Id { get; set; }
+
+        [Display(Name="Account #")]
+        public string AccountNumber { get; set; }
+        [Display(Name="First Name")]
+        public string FirstName { get; set; }
+        [Display(Name="Last Name")]
+        public string LastName { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return string.Format("{0} {1}", this.FirstName, this.LastName);
+            }
+        }
+        [DataType(DataType.Currency)]
+        public decimal Balance { get; set; }
+    }
+}
