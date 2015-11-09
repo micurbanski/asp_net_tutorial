e5662e9353d85cd9e3829986d656ff77de044cc3
urb4nm <urb4nm@gmail.com>
2015-11-09 15:47:52 +0100

    Fixed test ContactFormSaysYes

--- AutomatedTellerMachine.Tests/UnitTest1.cs
+++ AutomatedTellerMachine.Tests/UnitTest1.cs
@@ -24,7 +24,7 @@ namespace AutomatedTellerMachine.Tests
         public void ContactFormSaysThanks()
         {
             var homeController = new HomeController();
-            var result = homeController.Contact("I love your bank") as ViewResult;
+            var result = homeController.Contact("I love your bank") as PartialViewResult;
             Assert.IsNotNull(result.ViewBag.TheMessage);
         }
 
